e43acb56ee140d44a552b80d6bf974fcf5dabe7a
hamayanhamayan <bz.programmer@gmail.com>
2020-03-09 19:02:30 +0900

    Enable extension to be loaded

--- __tests__/urlMaker.test.js
+++ __tests__/urlMaker.test.js
@@ -1,4 +1,4 @@
-import { getNextUrl } from '../urlMaker';
+import { getNextUrl } from '../chex/urlMaker';
 
 test('get next url', () => {
   expect(getNextUrl('https://atcoder.jp/contests/abc158/tasks/abc158_a')).toBe('https://atcoder.jp/contests/abc158/tasks/abc158_b');
