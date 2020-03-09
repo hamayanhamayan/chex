5b7afc9c95e7e3e7f782eada275db9fe45ffe4f3
hamayanhamayan <bz.programmer@gmail.com>
2020-03-09 21:34:08 +0900

    Support Pornhub

--- __tests__/urlMaker.test.js
+++ __tests__/urlMaker.test.js
@@ -2,8 +2,13 @@ import { getNextUrl, getPrevUrl } from '../chex/urlMaker';
 
 test('get next url', () => {
   expect(getNextUrl('https://atcoder.jp/contests/abc158/tasks/abc158_a')).toBe('https://atcoder.jp/contests/abc158/tasks/abc158_b');
+  expect(getNextUrl('https://jp.pornhub.com/video')).toBe('https://jp.pornhub.com/video?page=2');
+  expect(getNextUrl('https://jp.pornhub.com/video?page=2')).toBe('https://jp.pornhub.com/video?page=3');
+  expect(getNextUrl('https://jp.pornhub.com/video?c=111&o=cm&page=3')).toBe('https://jp.pornhub.com/video?c=111&o=cm&page=4');
 });
 
 test('get prev url', () => {
     expect(getPrevUrl('https://atcoder.jp/contests/abc158/tasks/abc158_b')).toBe('https://atcoder.jp/contests/abc158/tasks/abc158_a');
-  });
+    expect(getPrevUrl('https://jp.pornhub.com/video?page=2')).toBe('https://jp.pornhub.com/video');
+    expect(getPrevUrl('https://jp.pornhub.com/video?page=3')).toBe('https://jp.pornhub.com/video?page=2');
+});
--- chex/urlMaker.js
+++ chex/urlMaker.js
@@ -1,4 +1,6 @@
 let atcoder = /^https:\/\/atcoder\.jp\/contests\/(.+)\/tasks\/(.+)$/;
+let pornhub1 = /^(https:\/\/jp\.pornhub\.com\/(.*)\page=)(.*)$/
+let pornhub2 = /^https:\/\/jp\.pornhub\.com\/(.*)$/
 
 export function getNextUrl(url) {
     if (atcoder.test(url)) {
@@ -6,6 +8,16 @@ export function getNextUrl(url) {
         var num = url.slice(-1);
         num = String.fromCharCode(num.charCodeAt(0) + 1)
         url = prefix + num;
+    }
+    if (pornhub2.test(url)) {
+        if (!pornhub1.test(url)) {
+            url = url + '?page=2';
+        } else {
+            var res = url.match(pornhub1);
+            var prefix = res[1];
+            var num = parseInt(res[3]) + 1
+            url = prefix + String(num)
+        }
     }
      
     return url;
@@ -17,6 +29,13 @@ export function getPrevUrl(url) {
         var num = url.slice(-1);
         num = String.fromCharCode(num.charCodeAt(0) - 1)
         url = prefix + num;
+    }
+    if (pornhub1.test(url)) {
+        var res = url.match(pornhub1);
+        var prefix = res[1];
+        var num = parseInt(res[3]) - 1
+        if (num == 1) url = prefix.slice(0, -6)
+        else url = prefix + String(num)
     }
      
     return url;
